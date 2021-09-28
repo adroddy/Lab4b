6972dc950c3d39cf08b7dd3892973e110edb7737
adroddy <90008134+adroddy@users.noreply.github.com>
2021-09-28 22:17:14 +0000

    error fix

--- main.cs
+++ main.cs
@@ -3,25 +3,25 @@ using System;
 class Program {
   public static void Main (string[] args) {
     
-   int counter = 0;
-    int total = 0;
+   double counter = 0;
+    double total = 0;
 
     {
       for(counter)
       Console.WriteLine("Enter the number of cars");
-      int cars = Convert.ToInt32(Console.ReadLine());
+      double cars = Convert.ToDouble(Console.ReadLine());
       Console.WriteLine(cars);
-    
+
 
       Console.WriteLine("Enter the distance covered by int car");
-      int distance = Convert.ToInt32(Console.ReadLine());
+      double distance = Convert.ToDouble(Console.ReadLine());
       Console.WriteLine(distance);
       
       Console.WriteLine("Enter the time taken by int car");
-      int time = Convert.ToInt32(Console.ReadLine());
+      double time = Convert.ToDouble(Console.ReadLine());
       Console.WriteLine(time);
 
-      int speed = (distance / time );
+      double speed = (distance/time);
 
       Console.WriteLine("The speed of this car is {0} miles per hour", (distance / time));
 
@@ -30,7 +30,7 @@ class Program {
       counter++;
     }
 
-    double average = total / int cars;
+    double average = total / cars;
     
     Console.WriteLine("Average = " + average); 
     
